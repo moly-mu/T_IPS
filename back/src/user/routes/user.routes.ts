159165666b089ce54114a3c835ab8579649629dc
Davidb2303 <dballesteros2303@gmail.com>
2025-07-03 14:06:36 -0500

    Controller requests specialist and general routes

---
+++ back/src/user/routes/user.routes.ts
@@ -0,0 +1,11 @@
+import {registerUser} from "../controllers/auth.Register.controller";
+import {loginUser} from "../controllers/auth.Login.controller";
+import {requestSpecialist} from "../controllers/Specialist.Request.controller";
+
+// *Rutas de registrarse y de iniciar sesión del usuario
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+// *Ruta usuario para solicitar ser especialista
+router.post("/request-specialist", verifyToken, requestSpecialist);
+
