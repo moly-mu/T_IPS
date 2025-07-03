e74244753ada200e762b126c1e2b6a1b467654fb
Davidb2303 <dballesteros2303@gmail.com>
2025-07-03 15:39:25 -0500

    Modularizate and config project

---
+++ back/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./user/routes/user.routes";
+import adminProfRoutes from "./admin/routes/profesional.routes";
+import adminSpecialRoutes from "./admin/routes/specialty.routes";
+import adminPatientRoutes from "./admin/routes/patient.routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas de usuario
+app.use("/api", userRoutes); // /api/register, /api/login, /api/request-specialist
+
+// Rutas de administración
+app.use("/admin/profesional", adminProfRoutes);
+app.use("/admin/specialty", adminSpecialRoutes);
+app.use("/admin/patient", adminPatientRoutes);
+
+export default app;
