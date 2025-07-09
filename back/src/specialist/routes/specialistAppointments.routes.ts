3949e8fb35da96535d4bd66caab4fd9b002eaa52
Davidb2303 <dballesteros2303@gmail.com>
2025-07-09 16:42:36 -0500

    Fix and create Routes SpecialistProfile

---
+++ back/src/specialist/routes/specialistAppointments.routes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {getAppointments} from "../controllers/listappoinments.controller";
+import {validateToken} from "../middleware/authMiddleware";
+import { listSpecialistRequests } from "../controllers/listSpecialist.Request.controller";
+import { getProfile } from "../controllers/profile/listproSpecialist.controller";
+import { updateProfile} from "../controllers/profile/editproSpecialist.controller";
+
+const router = express.Router()
+
+router.get("/getAppointments",validateToken, getAppointments);
+router.get("/listSpecialist",validateToken,listSpecialistRequests);
+router.get("/getProfile",validateToken,getProfile);
+router.post("/editProfile",validateToken,updateProfile);
+
+
+
+
+export default router;
