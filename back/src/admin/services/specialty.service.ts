36d11397580ea954ae4d3637ae34e9608e77dc04
ArturoAgudelo05 <arturo10agudelo@gmail.com>
2025-07-17 17:00:10 -0500

    admin_newPrisma

--- back/src/admin/services/specialty.service.ts
+++ back/src/admin/services/specialty.service.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-console.log(prisma)
+// const prisma = new PrismaClient();
+// console.log(prisma)
 
-export async function createSpecialtyService(name: string) {
-  return await prisma.specialty.create({
-    data: { name },
-  });
-}
+// export async function createSpecialtyService(name: string) {
+//   return await prisma.specialty.create({
+//     data: { name },
+//   });
+// }
 
-export async function changeUserStatusService(userId: number, newStatus: string) {
-  return await prisma.profesional.update({
-    where: { id: userId },
-    data: { status: newStatus },
-  });
-}
+// export async function changeUserStatusService(userId: number, newStatus: string) {
+//   return await prisma.profesional.update({
+//     where: { id: userId },
+//     data: { status: newStatus },
+//   });
+// }
