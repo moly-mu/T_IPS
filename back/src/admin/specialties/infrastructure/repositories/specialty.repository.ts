bb0961fcd05220829db544136e72692e445a1fd0
Davidb2303 <dballesteros2303@gmail.com>
2025-08-01 12:47:21 -0500

    Otro merge

--- back/src/admin/specialties/infrastructure/repositories/specialty.repository.ts
+++ back/src/admin/specialties/infrastructure/repositories/specialty.repository.ts
@@ -1,33 +1,33 @@
-//src/admin/specialties/repositories/specialty.repository.ts
+// //src/admin/specialties/repositories/specialty.repository.ts
 
-import { PrismaClient } from "@prisma/client";
-import { SpecialtyEntity } from "../../domain/entities/";
-import { SpecialtyRepository } from "../../domain/repositories/";
+// import { PrismaClient } from "@prisma/client";
+// import { SpecialtyEntity } from "../../domain/entities/";
+// import { SpecialtyRepository } from "../../domain/repositories/";
 
-const prisma = new PrismaClient();
-export class GetAllSpecialtyPrismaRepository implements SpecialtyRepository {
-  async getAll(): Promise<SpecialtyEntity[]> {
-    return (await prisma.specialty.findMany({
-      orderBy: {
-        id: "asc",  
-      },
-      include: {
-        _count: {
-          select: { Appointment: true },
-        },
-      },
-    })).map(
-      (s) => 
-        new SpecialtyEntity(
-          s.id,
-          s.name,
-          s.status as SpecialtyStatus,
-          s.price,
-          s.service,
-          s.duration,
-          new Date(s.joinDate),
-          { Appointment: s._count.Appointment}
-        )
-    );
-  }
-}
+// const prisma = new PrismaClient();
+// export class GetAllSpecialtyPrismaRepository implements SpecialtyRepository {
+//   async getAll(): Promise<SpecialtyEntity[]> {
+//     return (await prisma.specialty.findMany({
+//       orderBy: {
+//         id: "asc",  
+//       },
+//       include: {
+//         _count: {
+//           select: { Appointment: true },
+//         },
+//       },
+//     })).map(
+//       (s) => 
+//         new SpecialtyEntity(
+//           s.id,
+//           s.name,
+//           s.status as SpecialtyStatus,
+//           s.price,
+//           s.service,
+//           s.duration,
+//           new Date(s.joinDate),
+//           { Appointment: s._count.Appointment}
+//         )
+//     );
+//   }
+// }
